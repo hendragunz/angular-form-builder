2edce608007bb3442136ecf4aecd5c064a0be8b7
Hendra Gunawan <developer.hendragunz@gmail.com>
2014-03-04 17:58:22 +0700

    add social fields #24

---
+++ app/inputs/twitter_input.rb
@@ -0,0 +1,12 @@
+class TwitterInput < SimpleForm::Inputs::StringInput
+  def input
+    input_html = input_html_options.dup
+    input_html[:as] ||= :string
+
+    html = template.content_tag(:div, :class => "input-group") do
+      template.content_tag(:span, :class => "input-group-addon") do
+        template.content_tag(:i, '', :class => "fa fa-twitter")
+      end + @builder.input_field(attribute_name, input_html)
+    end
+  end
+end
