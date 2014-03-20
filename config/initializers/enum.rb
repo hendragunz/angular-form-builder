e87854a9057066ed69eee4a2022e309848108559
Hendra Gunawan <developer.hendragunz@gmail.com>
2014-03-21 01:33:33 +0700

    no need field type Time

--- config/initializers/enum.rb
+++ config/initializers/enum.rb
@@ -1,7 +1,7 @@
 module Enum
   module FormField
     FIELD_TYPE = {
-      options: [:single_line, :boolean, :paragraph, :mcq, :rating, :number, :checkbox, :dropdown, :name, :address, :date, :datetime, :email, :time, :phone, :website, :price, :statement,
+      options: [:single_line, :boolean, :paragraph, :mcq, :rating, :number, :checkbox, :dropdown, :name, :address, :date, :datetime, :email, :phone, :website, :price, :statement,
                 :facebook, :twitter, :file, :picture_choice, :section, :range, :radio, :question_group],
       default: :single_line
     }
